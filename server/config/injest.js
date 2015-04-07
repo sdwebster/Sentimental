445a55137f779f27533c285444717f4d33002501
Mark Macdonald <mjemacdonald@gmail.com>
2015-04-07 11:55:43 -0700

    (fix) Set up local database correctly

--- server/config/injest.js
+++ server/config/injest.js
@@ -1,32 +1,32 @@
-var http = require('http');
+// var http = require('http');
 
-var searchTerm = 'BP';
-var beginDate = '20000101';
-var endDate = '20150406';
-var page = 0;
-var apiKey = '';
+// var searchTerm = 'BP';
+// var beginDate = '20000101';
+// var endDate = '20150406';
+// var page = 0;
+// var apiKey = 'e1881c6d2f420a90fc142ecf575188eb:1:71772916';
 
-var nytEndpoint = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?fq=headline:\"' + searchTerm + '\"&begin_date=' + beginDate + '&end_date=' + endDate + '&page=' + page + '&api-key=' + apiKey;
+// var nytEndpoint = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?fq=headline:\"' + searchTerm + '\"&begin_date=' + beginDate + '&end_date=' + endDate + '&page=' + page + '&api-key=' + apiKey;
 
-console.log(nytEndpoint);
+// console.log(nytEndpoint);
 
-// Gets first page of results and check metadata for further pages
+// // Gets first page of results and check metadata for further pages
 
-http.get(nytEndpoint, function(res) {
-  var body = '';
+// http.get(nytEndpoint, function(res) {
+//   var body = '';
 
-  res.on('data', function(chunk) {
-    body += chunk;
-  });
+//   res.on('data', function(chunk) {
+//     body += chunk;
+//   });
 
-  res.on('end', function() {
-    var nytData = JSON.parse(body).response;
-    console.log('Got response: ' + nytData);
-    for(key in nytData.meta){
-      console.log(key);
-    }
-  });
+//   res.on('end', function() {
+//     var nytData = JSON.parse(body).response;
+//     console.log('Got response: ' + nytData);
+//     for(key in nytData.meta){
+//       console.log(key);
+//     }
+//   });
 
-}).on('error', function(err){
-  console.log("Got error: " + err);
-});
+// }).on('error', function(err){
+//   console.log("Got error: " + err);
+// });
