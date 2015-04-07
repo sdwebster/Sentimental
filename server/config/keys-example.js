46662dc0d20e06be86b9766326b4bda72a3fe209
Steve <stephendwebster@gmail.com>
2015-04-07 14:29:18 -0700

    Create object to store news source api keys

--- server/config/keys-example.js
+++ server/config/keys-example.js
@@ -2,10 +2,20 @@
 
 // $ mysql.server start
 // $ mysql -uroot
-// mysql> create database sentimentalDev
+// mysql> create database sentimentalDev;
 // mysql> grant all on sentimentalDev.* to 'myNewUsername' identified by 'myNewPassword';
+// mysql> flush privileges;
 
 module.exports.mysqlUser = {
   username: 'myNewUsername',
   password: 'myNewPassword'
+}
+
+module.exports.sourceKeys = {
+  // [news source name *exactly as it appears in database*]
+  'New York Times': {
+    // [keys]
+    'password':
+    'AOEUDHTNS'
+  }
 }
