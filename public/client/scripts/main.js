003d5ced950d7dded97bf124c11515ba4e099de1
Steve <stephendwebster@gmail.com>
2015-04-08 17:19:35 -0700

    Put 'require' statements on hold for now

--- public/client/scripts/main.js
+++ public/client/scripts/main.js
@@ -1,26 +1,26 @@
 /*global require*/
 'use strict';
 
-require.config({
-    shim: {
-        bootstrap: {
-            deps: ['jquery'],
-            exports: 'jquery'
-        }
-    },
-    paths: {
-        jquery: '../bower_components/jquery/dist/jquery',
-        backbone: '../bower_components/backbone/backbone',
-        underscore: '../bower_components/lodash/dist/lodash',
-        bootstrap: '../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap'
-    }
-});
+// require.config({
+//     shim: {
+//         bootstrap: {
+//             deps: ['jquery'],
+//             exports: 'jquery'
+//         }
+//     },
+//     paths: {
+//         jquery: '../bower_components/jquery/dist/jquery',
+//         backbone: '../bower_components/backbone/backbone',
+//         underscore: '../bower_components/lodash/dist/lodash',
+//         bootstrap: '../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap'
+//     }
+// });
 
-require([
-    'backbone'
-], function (Backbone) {
-    Backbone.history.start();
-});
+// require([
+//     'backbone'
+// ], function (Backbone) {
+//     Backbone.history.start();
+// });
 
 /***********/
 console.log('running main');
