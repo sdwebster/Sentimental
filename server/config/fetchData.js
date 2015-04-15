5e8233d4d5faaf97eba90feacabcba71c62add8d
Mark Macdonald <mjemacdonald@gmail.com>
2015-04-15 10:27:07 -0700

    (feat) Add bluebird promise handling to fetchData

--- server/config/fetchData.js
+++ server/config/fetchData.js
@@ -1,45 +1,61 @@
 var keys = require('./keys.js');
 var Article = require('./models/articleModel.js');
-var Articles = require('./collections/articles.js');
 var Keyword = require('./models/keywordModel.js');
-// var Keywords = require('./collections/keywords.js');
-// var Source = require('./models/sourceModel.js');
-// var Sources = require('./collections/sources.js');
-
-// new Keyword('id')
-//   .query('where', 'word', '=', 'BP')
-//   .fetchAll
-
-// // new keywords({word: 'BP'})
-
+var Source = require('./models/sourceModel.js');
+var bluebird = require('bluebird');
 
 var fetchData = function (req, res){
   var startDate = req.query.startDate || 00000000;
   var endDate = req.query.endDate || new Date();
   var timePeriod = req.query.timePeriod;
-  // var keyword = fetchID('keywords', req.query.keyword);
-  // var source = fetchID('sources', req.query.source);
 
-  new Keyword({'name': 'Jeb Bush'})
-    .fetch()
-    .then(function(model) {
-      console.log(model.get('id'));
-    });
+  var keywordId = function(){
+    return new Keyword({'word': 'Jeb Bush'})
+    .fetch();
+  };
 
-  console.log('This should be 2, above us');
+  var sourceId = function(){
+    return new Source({'name': 'newyorktimes'})
+    .fetch();
+  };
 
-  new Article()
-    .query('where', 'published', '>', startDate )
-    .query('where', 'published', '<', endDate )
-    // .query('where', 'source', '=', source )
-    // .query('where', 'word', '=', keyword )
-    .fetchAll()
+  bluebird.join(keywordId(), sourceId())
+    .then(function(array){
+      console.log('our promises are back: ', array[0].get('id'), 'whats this?', array[1].get('id'));
+      return new Article()
+        .query('where', 'published', '>', startDate )
+        .query('where', 'published', '<', endDate )
+        .query('where', 'word', '=', array[0].get('id') )
+        .query('where', 'source', '=', array[1].get('id') )
+        .fetchAll()
+    })
     .then(function(articles) {
       res.send(articles.toJSON());
     }).catch(function(error) {
       console.log(error);
       res.send('An error occured, please ensure that you request a keyword and source.');
     });
+}
+
+
+module.exports = fetchData;
+
+ 
+
+
+
+// var fetchID = function (table, value){
+//   //TODO refactor with lookup to keyword/source tables
+//   if (table = 'keywords'){
+//     new Keywords({word: 'BP'})
+//   }
+
+  // if (table === 'keywords' && value === 'BP'){
+  //     return 1;
+  // } else if (table === 'sources' && value === 'newyorktimes'){
+  //     return 1;
+  // }
+// };
 
   // if (timePeriod) {
   //     res.send('We\'ll get to this');
@@ -57,19 +73,3 @@ var fetchData = function (req, res){
   //     //     });
   // } else {
   // }
-}
-
-var fetchID = function (table, value){
-  //TODO refactor with lookup to keyword/source tables
-  if (table = 'keywords'){
-    new Keywords({word: 'BP'})
-  }
-
-  // if (table === 'keywords' && value === 'BP'){
-  //     return 1;
-  // } else if (table === 'sources' && value === 'newyorktimes'){
-  //     return 1;
-  // }
-};
-
-module.exports = fetchData;
