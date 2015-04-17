49d5b17137e6cfbca7cf87551823d884d45ed2b8
Mark Macdonald <mjemacdonald@gmail.com>
2015-04-17 15:32:27 -0700

    (feat) Adds dot expansion on hoverover

--- public/client/views/QueryView.js
+++ public/client/views/QueryView.js
@@ -36,36 +36,37 @@ var QueryView = Backbone.View.extend({
       .attr("class", "tooltip")
       .style("opacity", 1);
 
-    console.log("Possible jQuery trans: ", this.parent.selectAll(".dot").transition);
-    console.log("Possible D3 trans: ", tooltip.transition);
     var dataPoint = this.parent.selectAll(".dot");
+    console.log("datapoint: ", dataPoint)
 
     var dataPoints = dataPoint.data(this.model.get('articles'));
+    console.log("datapoints: ", dataPoints);
 
-      dataPoints.enter().append("circle")
+    dataPoints.enter().append("circle")
       .attr("class", "dot")
       .attr("r", 4)
       .attr("cx", this.xMap)
       .attr("cy", 900)
       .attr("opacity", 0.1)
       .style("fill", this.model.get('color')) 
-      .on("mouseover", function(d) {
-        console.log(d);
+      .on("mouseover", function(d, i) {
+        console.log(dataPoints[0][i]);
+        console.log(this);
         tooltip.transition()
           .duration(200)
           .style("opacity", .9);
         tooltip.html(d["url"])
           .style("left", (d3.event.pageX + 5) + "px")
           .style("top", (d3.event.pageY - 28) + "px");
-        dataPoints.transition()
-          .attr("r", 7);
+        d3.select(this).transition()
+          .attr("r", 10);
       })
       .on("mouseout", function(d) {
         tooltip.transition()
           .duration(500)
           .style("opacity", 0);
-        dataPoints.transition()
-          .attr("r", 3.5);
+        d3.select(this).transition()
+          .attr("r", 4);
         });
 
       dataPoints.transition().duration(2000).attr("cy", this.yMap)
