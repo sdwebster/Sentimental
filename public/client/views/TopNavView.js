1e79acbc1bc5716de5e824324c108eb917043830
Mark Macdonald <mjemacdonald@gmail.com>
2015-04-20 21:09:54 -0700

    (feat) Adds TopNav coloring

--- public/client/views/TopNavView.js
+++ public/client/views/TopNavView.js
@@ -1,12 +1,12 @@
 var TopNavView = Backbone.View.extend({
   
-  tagName: 'div class=topNav',
+  tagName: 'div',
 
   initialize: function() {
     this.render();
   },
 
-  template: _.template('<nav class="navbar navbar-default navbar-fixed-top"><div class="container"><div class="navbar-header"><a class="navbar-brand" href="#">SENTIMENTAL</a></div><p class="navbar-text navbar-right"><a href="#" class="navbar-link">Sandbox</a></p><p class="navbar-text navbar-right"><a href="#" class="navbar-link">About Us</a></p><p class="navbar-text navbar-right"><a href="#" class="navbar-link">Stories</a></p></div></nav>'),
+  template: _.template('<nav class="navbar navbar-default navbar-fixed-top topNav"><div class="container"><div class="navbar-header"><a class="navbar-brand" href="#">SENTIMENTAL</a></div><p class="navbar-text navbar-right"><a href="#" class="navbar-link">Sandbox</a></p><p class="navbar-text navbar-right"><a href="#" class="navbar-link">About Us</a></p><p class="navbar-text navbar-right"><a href="#" class="navbar-link">Stories</a></p></div></nav>'),
 
   render: function(){
     return this.$el.html(this.template());
