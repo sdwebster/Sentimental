cc576257c5ead3e6409d693988dd40e0c3ee8844
Mark Macdonald <mjemacdonald@gmail.com>
2015-04-21 09:24:23 -0700

    (fix) Footer syling

--- public/client/views/FooterView.js
+++ public/client/views/FooterView.js
@@ -6,7 +6,7 @@ var FooterView = Backbone.View.extend({
     this.render();
   },
 
-  template: _.template('<nav class="navbar navbar-default navbar-fixed-bottom topNav"><div class="container"><div class="navbar-footer"></div><p class="navbar-text navbar-right">Created by Optimistic Botanists  © 2015</p></div></nav>'),
+  template: _.template('<nav class="navbar navbar-default navbar-fixed-bottom bottomNav"><div class="container"><div class="navbar-footer"></div><p class="navbar-text navbar-right">Created by Optimistic Botanists  © 2015</p></div></nav>'),
 
   render: function(){
     return this.$el.html(this.template());
